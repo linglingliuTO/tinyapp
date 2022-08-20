c4bc1e37cd86b6bcc75f315e9f17a1400dabf27b
linglingliuTO <linglingliu.to@gmail.com>
2022-08-20 19:57:15 +0000

    added function to redirect user to new short url

--- express_server.js
+++ express_server.js
@@ -37,6 +37,32 @@ app.listen(PORT, () => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  //console.log(req.body); // Log the POST request body to the console
+  let newID = generateRandomString()
+  urlDatabase[newID] = req.body["longURL"]
+
+  const templateVars = { id: newID, longURL: urlDatabase[newID] };
+  res.render("urls_show", templateVars);
+
+
+  
 });
+
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
+});
+
+
+function generateRandomString() {
+  let result = ""
+  let alphabet = {1:"a", 2:"b", 3:"c", 4:"d", 5:"e", 6:"f", 7:"g", 8:"g", 9:"h"}
+  randNum = () => Math.round((Math.random() * (9 - 1) + 1), 0)
+  for (var i = 0; i<3; i++ ) {
+    j = randNum()
+    result+=(j)
+    result+=(alphabet[j])
+  }
+  return result
+}
