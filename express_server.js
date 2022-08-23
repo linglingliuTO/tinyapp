8559a80a5530358caebccf5e2877998e1325216a
linglingliuTO <linglingliu.to@gmail.com>
2022-08-23 23:43:37 +0000

    added user fucntions to limit access

--- express_server.js
+++ express_server.js
@@ -6,91 +6,205 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+// databases 
 const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
+  "aJ48lW": {
+    id: "aJ48lW",
+    email: "linglingliu344@hotmail.com",
+    password: "123456",
   },
+  "aJ48lx": {
+    id: "aJ48lx",
+    email: "linglingliu@hotmail.com",
+    password: "123456",
+  },
+  "aJ48ly": {
+    id: "aJ48ly",
+    email: "lingling@hotmail.com",
+    password: "1234567",
+  },
+
 };
 
-
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+  c6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lx",
+  },
+  e3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lx",
+  }
+
 };
 
 
 
 app.get("/urls", (req, res) => {
-  const user = users[req.cookies["user_id"]]
-  const templateVars = { urls: urlDatabase, user };
+  const userID = req.cookies["user_id"]
+  const user = users[userID]
+  const urls = urlsForUser(userID, urlDatabase)
+
+  if (!user) {
+    res.redirect("/login")
+
+  }
+
+  const templateVars = { urls, user };
   res.render("urls_index", templateVars);
 
 });
 
+
 app.get("/urls/new", (req, res) => {
   const user = users[req.cookies["user_id"]]
-  const templateVars = {  user };
+  const templateVars = { user };
+  if (!user) {
+    res.redirect("/login")
+  }
   res.render("urls_new", templateVars);
 });
 
 
 app.get("/urls/:id", (req, res) => {
-  const user = users[req.cookies["user_id"]]
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id],  user };
+  const userID = req.cookies["user_id"]
+  const user = users[userID]
+  const urls = urlsForUser(userID, urlDatabase)
+
+
+  if (!user) {
+    res.send("You are not logged in")
+    return
+  }
+  if (!Object.keys(urlDatabase).includes(req.params.id)) {
+    res.send("This link doesn't exist")
+    return
+  }
+
+  if (!Object.keys(urls).includes(req.params.id)) {
+    res.send("you don't have permission to access this")
+    return
+  }
+
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]["longURL"], user };
   res.render("urls_show", templateVars);
 });
 
+
+
+
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
+  const longURL = urlDatabase[req.params.id].longURL
+  if (longURL === undefined) {
+    res.send("Error: short url not found")
+  }
   res.redirect(longURL);
-  const user = users[req.cookies["user_id"]]
-  const templateVars = { urls: urlDatabase,  user };
-  res.render("urls_index", templateVars);
+
 });
 
 app.get("/register", (req, res) => {
   const user = users[req.cookies["user_id"]]
-  const templateVars = {  user };
-  res.render("register", templateVars);
+  const templateVars = { user };
+  if (user) {
+    res.redirect("/urls")
+  } else {
+    res.render("register", templateVars);
+  }
 });
 
 app.get("/login", (req, res) => {
   const user = users[req.cookies["user_id"]]
   const templateVars = { user };
-  res.render("login", templateVars);
-
+  if (user) {
+    res.redirect("/urls")
+  } else {
+    res.render("login", templateVars);
+  }
 });
 
+//---------posts ----------------------//
 
 app.post("/urls", (req, res) => {
-  const templateVars = { id: newID, longURL: urlDatabase[newID] };
-  res.render("urls_show", templateVars);
+  const user = users[req.cookies["user_id"]]
+  const databaseID = generateRandomString()
+  if (user) {
+    urlDatabase[databaseID] = {
+      "longURL": req.body["longURL"],
+      "userID": user.id
+    }
+    res.redirect("/urls")
+  } else {
+    res.send("error please login first")
+  }
 });
 
-
 app.post("/urls/:id/delete", (req, res) => {
+
+  const userID = req.cookies["user_id"]
+  const user = users[userID]
+  const urls = urlsForUser(userID, urlDatabase)
+
+
+  if (!user) {
+    res.send("You are not logged in")
+    return
+  }
+  if (!Object.keys(urlDatabase).includes(req.params.id)) {
+    res.send("This link doesn't exist")
+    return
+  }
+
+  if (!Object.keys(urls).includes(req.params.id)) {
+    res.send("you don't have permission to access this")
+    return
+  }
+
   delete urlDatabase[req.params.id]
   res.redirect("/urls");
 
 });
 
 app.post("/urls/:id/update", (req, res) => {
-  const user = users[req.cookies["user_id"]]
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id],  user };
+  const userID = req.cookies["user_id"]
+  const user = users[userID]
+  const urls = urlsForUser(userID, urlDatabase)
+
+  if (!user) {
+    res.send("You are not logged in")
+    return
+  }
+  if (!Object.keys(urlDatabase).includes(req.params.id)) {
+    res.send("This link doesn't exist")
+    return
+  }
+
+  if (!Object.keys(urls).includes(req.params.id)) {
+    res.send("you don't have permission to access this")
+    return
+  }
+
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user };
   res.render("urls_show", templateVars);
 
 });
 
+
+
 app.post("/urls/:id/updateData", (req, res) => {
-  urlDatabase[req.params.id] = req.body["updatedlongURL"]
+  urlDatabase[req.params.id].longURL = req.body["updatedlongURL"]
   res.redirect("/urls");
-
-
 });
 
-
+//---------posts login and registers----------------------//
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id")
@@ -106,68 +220,58 @@ app.post("/register", (req, res) => {
   } else if (req.body["password"] === "") {
     res.status(400)
     res.json({ error: "password cannot be blank" })
-  } else if (users [getUserByEmail(users, req.body["email"])] !== undefined) {
+  } else if (users[getUserByEmail(users, req.body["email"])] !== undefined) {
     res.status(400)
     res.json({ error: "Username already exist" })
   } else {
 
 
-  let userRandomID = generateRandomString()
-  users[userRandomID] = {
-    "id": userRandomID,
-    "email": req.body["email"],
-    "password": req.body["password"]
-  }
+    let userRandomID = generateRandomString()
+    users[userRandomID] = {
+      "id": userRandomID,
+      "email": req.body["email"],
+      "password": req.body["password"]
+    }
 
-  res.cookie("user_id", userRandomID);
-  res.redirect("/urls");
+    res.cookie("user_id", userRandomID);
+    res.redirect("/urls");
   }
 })
 
 app.post("/login", (req, res) => {
-let userID = getUserByEmail(users, req.body["email"])
-console.log(userID)
+  let userID = getUserByEmail(users, req.body["email"])
+
   if ((req.body["email"]) === "") {
     res.status(403)
     res.json({ error: "Username cannot be blank" })
   } else if (req.body["password"] === "") {
-    console.log(userID)
+
     res.status(403)
     res.json({ error: "Password cannot be blank" })
-  }  else if (userID === "") {
+  } else if (userID === "") {
     res.status(403)
     res.json({ error: "Username does not exist" })
-  }  else if  (users[userID].password !== req.body["password"]) {
-    console.log(users[userID].password)
+  } else if (users[userID].password !== req.body["password"]) {
+
     res.status(403)
     res.json({ error: "Password is incorrect" })
-
   } else {
 
-  res.cookie("user_id", userID);
-  res.redirect("/urls");
+    res.cookie("user_id", userID);
+    res.redirect("/urls");
 
   }
 
-  }
+}
 
 )
-
-
-
-
-
-
-
-
-
-
-
+// listen // 
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
 });
 
+// functions // 
 
 const generateRandomString = () => {
   let result = ""
@@ -190,3 +294,17 @@ const getUserByEmail = (object, value) => {
   }
   return result
 }
+
+
+
+const urlsForUser = (userId) => {
+  const urls = {}
+  for (const id in urlDatabase) {
+    const url = urlDatabase[id]
+    if (url.userID === userId) {
+      urls[id] = url
+    }
+  }
+
+  return urls
+}  
